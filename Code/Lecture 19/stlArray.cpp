e2e3c701444a63223e3111757f91d9a1b7c10590
easyvansh <vansh2@ualberta.ca>
2023-07-05 12:04:21 +0530

    Implementing STL Array

---
+++ Code/Lecture 19/stlArray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+// STL array is simmilar to basic static array
+
+int main(){
+
+    int basic[3] = {1,2,3};
+
+    array<int,4> a = {1,2,3,4};
+
+    int size = a.size();
+
+    for (int i = 0; i < size;i++){
+        cout<<a[i]<<endl;
+    }
+        cout<<"Element at 2nd Index"<<a.at(2)<<endl;
+}
+
